93c050f8b1457fd4060b848dc8a3e0b21f220080
AlexVIM1 <AlexVIM86@gmail.com>
2020-08-13 15:05:05 +0300

    Release v1.5-3 package

--- src/workspace.cpp
+++
@@ -1,9 +0,0 @@
-#include "workspace.h"
-
-files::files() : fontSize(12), nowFontSize(12), prevIndex(0) { }
-files * getFile = new files();
-files::~files() { delete getFile; }
-
-theme::theme() { }
-theme * getTheme = new theme();
-theme::~theme()  { delete getTheme; }
